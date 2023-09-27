f35558a3d81bbaa69ac304bc71c71a54a8d67e41
Richard Steinmetz <richard@steinmetz.cloud>
2023-09-27 12:21:03 +0200

    feat: add support for twofactor_totp
    Signed-off-by: Richard Steinmetz <richard@steinmetz.cloud>

--- remote.sh
+++ remote.sh
@@ -394,6 +394,7 @@ install_enable_app "$TEXT_BRANCH" text
 install_enable_app "$VIEWER_BRANCH" viewer
 install_enable_app "$ZIPPER_BRANCH" files_zip
 install_enable_app "$TWOFACTORWEBAUTHN_BRANCH" twofactor_webauthn
+install_enable_app "$TWOFACTORTOTP_BRANCH" twofactor_totp
 
 # Clear cache
 cd /var/www/nextcloud || exit
